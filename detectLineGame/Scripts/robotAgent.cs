ec5164e152b17e8b9402ab59fc1b8ecfc2f3bfe9
soya <soyamilk432@gmail.com>
2021-03-21 01:50:28 +0900

    이제 n을 누르면 에이전트는 랜덤으로 모서리를 바라봅니다. 라인을 내 뜻대로 조종하는 코드는 아직 작성중....

--- detectLineGame/Scripts/robotAgent.cs
+++ detectLineGame/Scripts/robotAgent.cs
@@ -4,18 +4,38 @@ using Unity.MLAgents.Sensors;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.Linq;
 
 
-//카메라의 범위는 -90에서 90사이로
-//일단 첫번째 목표는
-//카메라를 랜덤으로 움직이면서
-//막대기를 바닥에 붙이면서
-//막대기의 각도를 랜덤으로 움직이는 것으로 해보자
 public class robotAgent : Agent
 {
-    public GameObject camera;
-    public GameObject Line;
+    const float stadium_horizontal = 5f;
+    const float stadium_vertical = 5f;
+    const float line_max_z_lot = 90f;//90
+    const float line_min_z_lot = 270f;//-90
+    const float line_max_y_pos = 200;
+    const float line_min_y_pos = -200f;
+    const float line_max_x_pos = 200;
+    const float line_min_x_pos = -200f;
+    const float line_lot_unit = 5f;
+    const float line_move_unit = 10f;
+
+    public GameObject camera_pack;
+    public GameObject camera_target;
+    public GameObject RedLine;
+    public GameObject BlackLine;
+
+    private List<string> cam_target_move_mode_list = new List<string>();
+    private RectTransform redLine_rectTransform;
+    private RectTransform blackLine_rectTransform;
+
+    private void Start()
+    {
+        redLine_rectTransform = RedLine.GetComponent<RectTransform>();
+        blackLine_rectTransform = BlackLine.GetComponent<RectTransform>();
+    }
+
     public override void CollectObservations(VectorSensor sensor)
     {
 
@@ -23,21 +43,127 @@ public class robotAgent : Agent
 
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
+        var act0 = actionBuffers.DiscreteActions[0];
+        var nextMode = 0;
 
+        switch (act0)
+        {
+            case 1:
+                nextMode = 1;//카메라 타겟 오브젝트가 모서리를 따라 움직임
+                break;
+            case 2:
+                nextMode = 2;//추정 라인을 조작함
+                break;
+        }
+
+        if(nextMode == 1)
+        {
+            Move_Init_setting();
+        }
+
+        if(nextMode == 2)
+        {
+            var act1 = actionBuffers.DiscreteActions[1];
+            var linePos = redLine_rectTransform.anchoredPosition;
+            var lineRot = Mathf.Round(redLine_rectTransform.eulerAngles.z);
+            if (act1==8)
+            {
+                if (linePos.y < line_max_y_pos && linePos.x < line_max_x_pos)
+                    redLine_rectTransform.anchoredPosition = new Vector3(linePos.x,linePos.y, 0f) + redLine_rectTransform.up * line_move_unit;
+            }
+            else if(act1 == 2)
+            {
+                if (linePos.y > line_min_y_pos && linePos.x > line_min_x_pos)
+                    redLine_rectTransform.anchoredPosition = new Vector3(linePos.x, linePos.y, 0f) - redLine_rectTransform.up * line_move_unit;
+            }
+            else if(act1 == 4)
+            {
+                if(lineRot > line_min_z_lot || lineRot < line_max_z_lot)
+                    redLine_rectTransform.eulerAngles += new Vector3(0f, 0f, line_lot_unit);
+                else if(lineRot == line_min_z_lot)
+                    redLine_rectTransform.eulerAngles += new Vector3(0f, 0f, line_lot_unit);
+
+            }
+            else if (act1 == 6)
+            {
+                if (lineRot > line_min_z_lot || lineRot < line_max_z_lot)
+                    redLine_rectTransform.eulerAngles -= new Vector3(0f, 0f, line_lot_unit);
+                else if(lineRot==line_max_z_lot)
+                    redLine_rectTransform.eulerAngles -= new Vector3(0f, 0f, line_lot_unit);
+            }
+            blackLine_rectTransform.position = redLine_rectTransform.position;
+            blackLine_rectTransform.rotation = redLine_rectTransform.rotation;
+        }
     }
 
     public override void OnEpisodeBegin()
     {
+        cam_target_move_mode_list.Clear();
+        cam_target_move_mode_list.Add("fix_horizen");
+        cam_target_move_mode_list.Add("fix_vertical");
+        Move_Init_setting();
+    }
 
+    public void Move_Init_setting()
+    {
+        var randomInt_move_target_mode = Random.Range(0, cam_target_move_mode_list.Count());
+        if (randomInt_move_target_mode == 0)
+        {
+            var random_figure_for_vertical = Random.Range(-(stadium_vertical / 2f), +(stadium_vertical / 2f));
+            camera_target.transform.position = new Vector3(random_figure_for_vertical, 0f, 2.5f);
+        }
+        else
+        {
+            var random_figure_for_horizental = Random.Range(-(stadium_horizontal / 2f), +(stadium_horizontal / 2f));
+            camera_target.transform.position = new Vector3(2.5f, 0f, random_figure_for_horizental);
+        }
+
+        this.transform.position = new Vector3(Random.Range(-(stadium_vertical / 2f), +(stadium_vertical / 2f)), 0f, Random.Range(-(stadium_horizontal / 2f), +(stadium_horizontal / 2f)));
+        camera_pack.transform.LookAt(camera_target.transform.position);
+        var random_x_lot_for_camera_pack = new Vector3(Random.Range(-30f, 30f), 0f, 0f);
+        camera_pack.transform.eulerAngles = camera_pack.transform.eulerAngles + random_x_lot_for_camera_pack;
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
         var DiscreteActionsout = actionsOut.DiscreteActions;
         DiscreteActionsout[0] = 0;
+        DiscreteActionsout[1] = 0;
         if (Input.GetKey(KeyCode.N))
         {
             DiscreteActionsout[0] = 1;
         }
+        if(Input.GetKey(KeyCode.W))
+        {
+            DiscreteActionsout[0] = 2;
+            DiscreteActionsout[1] = 8;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            DiscreteActionsout[0] = 2;
+            DiscreteActionsout[1] = 2;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            DiscreteActionsout[0] = 2;
+            DiscreteActionsout[1] = 4;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            DiscreteActionsout[0] = 2;
+            DiscreteActionsout[1] = 6;
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            DiscreteActionsout[0] = 2;
+            DiscreteActionsout[1] = 7;
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            DiscreteActionsout[0] = 2;
+            DiscreteActionsout[1] = 9;
+        }
+
+
     }
 }
